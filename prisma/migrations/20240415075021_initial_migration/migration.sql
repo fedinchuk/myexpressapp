7700d74b3e1bc840fc92890e6deb614f32769d09
Vasyl Fedinchuk <vasyl.fedinchuk@gmail.com>
2024-04-15 09:52:57 +0200

    Use Postrgres instead of sqlite

---
+++ prisma/migrations/20240415075021_initial_migration/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
